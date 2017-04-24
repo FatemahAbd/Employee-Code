e95dc16eda7cf216bcf66045b31f13d2dbdae9c5
fatemah abdelaziz <fabdelaziz@smail.ucas.edu.ps>
2017-04-24 21:34:31 +0300

    Update EmployeeTester.java

--- EmployeeTester.java
+++ EmployeeTester.java
@@ -1,6 +1,7 @@
 public class EmployeeTester {
 
     public static void main(String[] args) {
+        //we make an object from Emlpoyee class and then we apply the methods that we have done in the previous class
         Employee fatemah= new Employee("Hacker,fatemah!",50000);
         System.out.println("Employee Name is " + fatemah.getName());
         System.out.println("Employee Salary is " + fatemah.getSalary());
